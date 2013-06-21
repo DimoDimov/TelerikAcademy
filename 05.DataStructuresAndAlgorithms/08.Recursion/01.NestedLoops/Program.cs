93b18a9300815024e0059f522f176dacccbe6977
DimoDimov <dimo.ivanov.dimov@gmail.com>
2013-06-21 07:25:12 +0300

    Uplodaded Recursion

---
+++ 05.DataStructuresAndAlgorithms/08.Recursion/01.NestedLoops/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _01.NestedLoops
+{
+    using System;
+    using System.Linq;
+
+    /*
+     * Write a recursive program that simulates the execution of n nested loops from 1 to n. Examples:
+     * 
+     *          1 1           
+     * n=2  ->  1 2
+     *          2 1 
+     *          2 2
+     */
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] variation = new int[n];
+ 
+            ProcessNestedLoops(variation, 0);
+        }
+
+        private static void ProcessNestedLoops( int[] variation, int arrIndex)
+        {
+            if (arrIndex == variation.Length)
+            {
+                Print(variation);
+                return;
+            }
+
+            for (int i = 1, len = variation.Length; i <= len; i++)
+            {
+                variation[arrIndex] = i;
+                ProcessNestedLoops(variation, arrIndex + 1);
+            }
+        }
+
+        private static void Print(int[] variation)
+        {
+            Console.WriteLine("(" + String.Join(",", variation) + ")");
+        }
+    }
+}
---
+++ 05.DataStructuresAndAlgorithms/08.Recursion/04.Permutations/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _04.Permutations
+{
+    using System;
+    using System.Linq;
+
+    /*
+     * Write a recursive program for generating and printing 
+     * all permutations of the numbers 1, 2, ..., n for 
+     * given integer number n. Example:
+     * 
+     * n=3  {1, 2, 3}, {1, 3, 2}, {2, 1, 3},			
+     *       {2, 3, 1}, {3, 1, 2},{3, 2, 1}
+     */
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] permutation = new int[n];
+            
+            //memoization in additional array
+            bool[] usedNum = new bool[n+1];
+
+            ProcessNestedLoops(permutation, 0, usedNum);
+        }
+
+        private static void ProcessNestedLoops(int[] permutation, int arrIndex, bool[] usedNum)
+        {
+            if (arrIndex == permutation.Length)
+            {
+                Print(permutation);
+                return;
+            }
+
+            for (int i = 1, len = permutation.Length; i <= len; i++)
+            {
+                if (!usedNum[i])
+                {
+                    permutation[arrIndex] = i;
+                    usedNum[i] = true;
+                    ProcessNestedLoops(permutation, arrIndex + 1, usedNum);
+                    usedNum[i] = false;
+                }
+                
+            }
+        }
+
+        private static void Print(int[] permutation)
+        {
+            Console.WriteLine("(" + String.Join(",", permutation) + ")");
+        }
+    }
+}
