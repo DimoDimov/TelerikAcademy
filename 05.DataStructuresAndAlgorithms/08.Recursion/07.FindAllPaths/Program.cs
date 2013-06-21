4750483d24ce06082ff2d2ffc1cb2915b3d13ac4
DimoDimov <dimo.ivanov.dimov@gmail.com>
2013-06-21 15:01:52 +0300

    Recursion 7, 8

--- 05.DataStructuresAndAlgorithms/08.Recursion/07.FindAllPaths/Program.cs
+++ 05.DataStructuresAndAlgorithms/08.Recursion/07.FindAllPaths/Program.cs
@@ -1,39 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _06.CombinationsFromSet
+﻿namespace _07.FindAllPaths
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /*
+     *  Modify the above program to check whether a 
+     *  path exists between two cells without finding all 
+     *  possible paths. Test it over an empty 100 x 100 matrix.
+     */
+
     class Program
     {
-        static string[] set = new string[] { "test", "rock", "fun"};
-        static int size;
-        static string[] result;
+        static char[,] lab = new char[,]{
+                                            {' ', ' ', ' ', '*', ' ', ' ', ' '},
+                                            {'*', '*', ' ', '*', ' ', '*', ' '},
+                                            {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                                            {' ', '*', '*', '*', '*', '*', ' '},
+                                            {' ', ' ', ' ', ' ', ' ', ' ', 'e'},
+                                        };
 
-        static void Main(string[] args)
+        static List<Tuple<int, int>> path = new List<Tuple<int, int>>();
+
+        static void Main()
         {
-            Console.WriteLine("Set size: {0}. Enter subset size to generate combinations:", set.Length);
-            size = int.Parse(Console.ReadLine());
-            result = new string[size];
+            int startRow = 0;
+            int startCol = 0;
 
-            GenerateVariations(0, 0);
+            FindPath(startRow, startCol);
         }
-
-        private static void GenerateVariations(int index, int start)
+  
+        private static void FindPath(int startRow, int StartCol)
         {
-            if (index >= size)
+            if (!IsInLab(startRow, StartCol))
             {
-                Console.WriteLine(string.Join(", ", result));
+                
                 return;
             }
 
-            for (int i = start; i < set.Length; i++)
+            if (lab[startRow,StartCol] == '*')
             {
-                result[index] = set[i];
-                GenerateVariations(index + 1, i + 1);
+                return;
             }
+
+            if (lab[startRow, StartCol] == 'e')
+            {
+                path.Add(new Tuple<int, int>(startRow, StartCol));
+                Print(path);
+                path.RemoveAt(path.Count - 1);
+            }
+
+            if (lab[startRow,StartCol] != ' ')
+            {
+                return;
+            }
+
+            path.Add(new Tuple<int, int>(startRow, StartCol));
+            lab[startRow, StartCol] = 's';
+
+            FindPath(startRow, StartCol - 1); //left recursively
+            FindPath(startRow - 1, StartCol); //up recursively
+            FindPath(startRow, StartCol + 1); //right recursively
+            FindPath(startRow + 1, StartCol); //down recursively
+
+            lab[startRow, StartCol] = ' ';
+            path.RemoveAt(path.Count - 1);
+        }
+
+        private static void Print(List<Tuple<int, int>> path)
+        {
+            Console.WriteLine("the way to exit is: ");
+            foreach (var step in path)
+            {
+                Console.Write("({0}, {1}) ", step.Item1, step.Item2);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+  
+        private static bool IsInLab(int row, int col)
+        {
+            bool rowIsInLab = row >= 0 && row < lab.GetLength(0);
+            bool colIsInLab = col >= 0 && col < lab.GetLength(1);
+
+            return rowIsInLab && colIsInLab;
         }
     }
 }
